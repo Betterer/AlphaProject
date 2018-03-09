22017e90c44e3183565f70bde0bc18e9f2b9498e
dingcan <423000389@qq.com>
2018-03-09 17:13:06 +0800

    测试修改内容提交

--- src/main/java/com/recycle/dao/StaffDao.java
+++ src/main/java/com/recycle/dao/StaffDao.java
@@ -8,6 +8,9 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface StaffDao extends CrudRepository<Staff, Long> {
 
+    //根据ID查询工作人员账号信息
+    Staff findStaffById(Integer id);
+
     //根据用户账号查询用户
     Staff findStaffByAccount(String account);
 }
